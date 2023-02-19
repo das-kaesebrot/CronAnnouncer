87a08ecbc5d0acc94ba3571beb8af1117a2edc7f
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-19 02:51:25 +0100

    [FEATURE] use new scheduler api

--- src/main/java/eu/kaesebrot/dev/CronAnnouncerPlugin.java
+++ src/main/java/eu/kaesebrot/dev/CronAnnouncerPlugin.java
@@ -6,6 +6,7 @@ import eu.kaesebrot.dev.utils.ScheduleConfigParser;
 import eu.kaesebrot.dev.utils.TickConverter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
@@ -17,7 +18,7 @@ public class CronAnnouncerPlugin extends JavaPlugin {
     private final TickConverter tickConverter = new TickConverter();
     private FileConfiguration config;
     private Logger logger;
-    private int subtaskSchedulerId;
+    private BukkitTask subtaskScheduler;
 
     Map<String, ScheduledMessage> scheduledMessages;
 
@@ -45,15 +46,9 @@ public class CronAnnouncerPlugin extends JavaPlugin {
 
     private void queueInitialScheduler() {
         var queueAheadDuration = Duration.of(1, ChronoUnit.HOURS);
+        var queuer = new ScheduledMessageTaskQueuer(this, scheduledMessages, queueAheadDuration);
 
-        subtaskSchedulerId =
-                this
-                .getServer()
-                .getScheduler()
-                .scheduleSyncRepeatingTask(
-                        this,
-                        new ScheduledMessageTaskQueuer(this, scheduledMessages, queueAheadDuration),
-                        0L, tickConverter.durationToTicks(queueAheadDuration));
+        subtaskScheduler = queuer.runTaskTimer(this, 0L, tickConverter.durationToTicks(queueAheadDuration));
     }
 
     private void cancelAllTasks() {
