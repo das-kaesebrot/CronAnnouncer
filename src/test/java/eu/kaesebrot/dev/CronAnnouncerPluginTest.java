219eefbadf336dde233fdec50333f388c408c1db
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-06-24 01:36:46 +0200

    [FEATURE] add initial unit test files

---
+++ src/test/java/eu/kaesebrot/dev/CronAnnouncerPluginTest.java
@@ -0,0 +1,13 @@
+package eu.kaesebrot.dev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CronAnnouncerPluginTest
+{
+    @Test
+    public void testMethod() {
+        assertTrue(true);
+    }
+}
